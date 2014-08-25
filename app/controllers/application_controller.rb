916179a3a5f62e87da2e34d5c5c85868c3eba5e5
agrinb <agrinb@gmail.com>
2014-08-25 19:39:31 -0400

    added custom headers to application.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  # protect_from_forgery with: :null_session
-  # private
+  protect_from_forgery with: :null_session
+  before_action :allow_cross_domain_access
+  def allow_cross_domain_access
+      response.headers["Access-Control-Allow-Origin"] = "*"
+      response.headers["Access-Control-Allow-Methods"] = "*"
+  end
 
-  # def current_user
-  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  # end
-  # helper_method :current_user
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
 end
