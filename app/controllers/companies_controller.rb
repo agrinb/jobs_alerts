e5ac095804b0027a826c1f95960e0018d5f45c5b
Alek <agrinb@gmail.com>
2014-12-16 20:55:04 -0500

    Added method for processing CL urls

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -12,39 +12,66 @@ class CompaniesController < ApplicationController
     @company.user = User.find_by(uid: company_params['uid'])
     @company.last_fetch = Nokogiri::HTML(open(company_params['url']))
     binding.pry
-      if @company.save
-        respond_to do |format|
-          if @company.save
+    if @company.save
+      respond_to do |format|
+        if @company.save
 
-            format.json { render :json => {:status => 200, :text => "ok"} }
+          format.json { render :json => {:status => 200, :text => "ok"} }
 
-          else
-            format.json { render json: @company.errors, status: :unprocessable_entity }
-          end
+        else
+          format.json { render json: @company.errors, status: :unprocessable_entity }
         end
       end
+    end
   end
 
   def show
-    company = Company.find(params[:id])
-    keywords = company.keywords
-    doc = Nokogiri::HTML(open(company.url))
+    @company = Company.find(params[:id])
+    keywords = @company.keywords
+    doc = Nokogiri::HTML(open(@company.url))
     a_nodes = doc.css("a")
     kwords = []
     keywords.each do |k|
        kwords << k.downcase
        kwords << k.capitalize
     end
-    doc_links = []
+    jobs_array = []
     kwords.each do |keyword|
       a_nodes.each do |node|
         if node.text.include?(keyword)
-          doc_links << { node.text => node['href'] }
+          jobs_array << { node.text => node['href'] }
         end
       end
     end
-    doc_links
+    process_jobs(jobs_array, @company)  
   end
+
+  def process_jobs(jobs_array, company)
+    if company.url.include?('craigslist.org/search')
+      process_cl_urls(jobs_array, @company.url)
+    else
+      process_other_urls(company)
+    end
+  end
+
+  def process_other_urls
+  end
+
+  def process_cl_urls(jobs_array, url)
+    processed_jobs = []
+    jobs_array.each do |job|
+      unless job.values.include?('http://')
+        base_url = url.split('/search').first
+        mod_url = base_url << job.values.first
+        modified_job = { job.keys.first => mod_url }
+      end
+      processed_jobs << modified_job
+    end
+    processed_jobs
+    binding.pry
+  end
+
+
 
 
   def company_params
