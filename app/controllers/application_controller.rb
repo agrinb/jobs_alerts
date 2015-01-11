fe279136d110afe0055e729cfb8769fada0a4597
Alek <agrinb@gmail.com>
2015-01-10 22:09:38 -0500

    Added more CORS codo to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,44 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   before_action :allow_cross_domain_access
+  helper_method :current_user
+  before_filter :current_user, :cors_preflight_check
+  after_filter :cors_set_access_control_headers
+
   def allow_cross_domain_access
       response.headers["Access-Control-Allow-Origin"] = "*"
       response.headers["Access-Control-Allow-Methods"] = "*"
   end
 
-  private
+# For all responses in this controller, return the CORS access control headers.
 
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  # If this is a preflight OPTIONS request, then short-circuit the
+  # request, return only the necessary headers and return an empty
+  # text/plain.
+
+  def cors_preflight_check
+    if request.method == :options
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = '*'
+      headers['Access-Control-Max-Age'] = '1728000'
+      render :text => '', :content_type => 'text/plain'
+    end
+  end
+
+  private
+  # get the user currently logged in
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
 end
+
