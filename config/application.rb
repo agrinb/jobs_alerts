b911857d1214dfea477cda56130832641707300e
Alek <agrinb@gmail.com>
2015-01-12 11:33:48 -0500

    Changed the application.rb to old code for testing cors requests

--- config/application.rb
+++ config/application.rb
@@ -16,23 +16,12 @@ Bundler.require(*Rails.groups)
 module Jobsapp
   class Application < Rails::Application
 
-  config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
-      allow do
-        origins '*'
-
-        resource '/cors',
-          :headers => :any,
-          :methods => [:post],
-          :credentials => true,
-          :max_age => 0
-
-        resource '*',
-          :headers => :any,
-          :methods => [:get, :post, :delete, :put, :options, :head],
-          :max_age => 0
-      end
-    end
-
+	  config.middleware.use Rack::Cors do
+	    allow do
+	      origins '*'
+	      resource '*', :headers => :any, :methods => [:get, :post, :options]
+	    end
+	  end
 
   end
 end
